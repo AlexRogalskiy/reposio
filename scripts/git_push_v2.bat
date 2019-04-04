1ca226a4b0e65e5f68f7ed2748428c91e5ecbdc0
AlexanderRogalskiy <alexander.rogalskiy@paragon-software.com>
2019-04-04 16:17:38 +0300

    Changeset to project structure

--- scripts/git_push_v2.bat
+++ scripts/git_push_v2.bat
@@ -18,9 +18,10 @@ SET t=%~nx1
 IF "%t:~0,1%" == "_" (
 	GOTO :EOF
 ) ELSE (
-	ECHO [41m Pushing to ["%1"] [0m
-	CD "%1" & CMD /c git add . & CMD /c git commit -m "Changeset to project structure" & CMD /c git push
+	GOTO :GIT
 )
 
-:END
+:GIT
+ECHO [41m Pushing to ["%1"] [0m
+CD "%1" & CMD /c git add . & CMD /c git commit -m "Changeset to project structure" & CMD /c git push
 ECHO [95m Push finished[0m
