ea725e555a8313b2e2086e65ab15f8f417be6c5c
AlexanderRogalskiy <alexander.rogalskiy@paragon-software.com>
2019-04-04 16:45:41 +0300

    Changeset to project structure

--- scripts/git_push_v2.bat
+++ scripts/git_push_v2.bat
@@ -6,11 +6,12 @@ SET current=%~n0
 SET parent=%~dp0
 
 :: GitHub Pushing
-ECHO [95m Pushing to "%parent%"[0m
+ECHO [94m Update to "%parent%"[0m
 
 PAUSE
 
 FOR /d %%i IN (%parent%*) DO call :PUSHTAG %%i
+
 GOTO END
 
 :PUSHTAG
@@ -22,11 +23,11 @@ IF "%t:~0,1%" == "_" (
 )
 
 :GIT
-ECHO --------------------------------------------------------------------------
-ECHO [41m Pushing to ["%1"] [0m
+ECHO [43m Pushing to repository ["%1"] [0m
 CD "%1" & CMD /c git add . & CMD /c git commit -m "Changeset to project structure" & CMD /c git push
-ECHO [95m Push finished[0m
+ECHO [43m Push finished[0m
 ECHO --------------------------------------------------------------------------
+GOTO :EOF
 
 :END
-ECHO [95m Pushing to "%parent%" finished[0m
+ECHO [94m Push to "%parent%" finished[0m
